4d8183e36e313bd65f76040f7c2837e7273f18c9
Ed Bice <edbice@gmail.com>
2025-09-03 15:25:26 -0700

    Fix Apps Script URL to match deployed web app

--- api/submit.js
+++ api/submit.js
@@ -17,3 +17,21 @@ export default async function handler(req, res) {
     bodyType: typeof req.body
   });
 }
+function submitForm() {
+  console.log('submitForm called - form is trying to submit');
+  console.log('answers object:', answers);
+  
+  const formData = new FormData();
+  
+  Object.keys(answers).forEach(key => {
+    const value = answers[key] || '';
+    console.log(`Adding to FormData: ${key} = ${value}`);
+    formData.append(key, value);
+  });
+
+  console.log('About to send POST request to:', APPS_SCRIPT_URL);
+  
+  typeLine('Submitting RSVP…', () => {});
+  
+  // ... rest of your existing fetchWithTimeout code
+}
